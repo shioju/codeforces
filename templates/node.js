260d0dc87c4a72a30588172dfd7b4c66d1bd7f6f
Sheng Hau <shioju12@gmail.com>
2020-08-26 10:23:43 +0800

    Add template for node.js

---
+++ templates/node.js
@@ -0,0 +1,30 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+  inputString = inputString.trim().split('\n').map(string => {
+    return string.trim();
+  });
+
+  main();
+});
+
+function readline() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  const t = parseInt(readline());
+  const nums = readline().split(" ").map(x => parseInt(x));
+  for (var i=0; i<t; i++) {
+  }
+}
